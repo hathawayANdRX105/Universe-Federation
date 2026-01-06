c22921d0668a23bc069fad4e966ddc15183df451
Hazelnoot <acomputerdog@gmail.com>
2026-01-05 23:04:26 -0500

    rewrite frontend randomId() function to produce a fixed-width output with greater entropy and compression

--- packages/frontend/src/utility/random-id.ts
+++ packages/frontend/src/utility/random-id.ts
@@ -1,15 +1,15 @@
 /*
- * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-FileCopyrightText: hazelnoot and other Sharkey contributors
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-const CHARS = 'abcdefghijklmnopqrstuvwxyz'; // CSSの<custom-ident>などで使われることもあるのでa-z以外使うな
+const MAX_ID = Math.pow(2, 50);
 
-export function randomId(length = 32, characters = CHARS) {
-	let result = '';
-	const charactersLength = characters.length;
-	for ( let i = 0; i < length; i++ ) {
-		result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
+/**
+ * Returns a non-cryptographically-secure 50-bit random ID encoded as a base-32 string.
+ * The output of this function is always 10 characters exactly, as 50 bits fits perfectly into 10 characters at 5 bits-per-character.
+ */
+export function randomId(): string {
+	const randomNum = Math.round(Math.random() * MAX_ID);
+	return randomNum.toString(26).padStart(10, '0');
 }
