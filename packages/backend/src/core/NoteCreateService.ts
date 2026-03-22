b1e012bb45faa15d4c25fd61bd0094d5a0c3f6f1
Hazelnoot <acomputerdog@gmail.com>
2026-03-21 22:52:42 -0400

    fix inverted access condition in NoteCreateService

--- packages/backend/src/core/NoteCreateService.ts
+++ packages/backend/src/core/NoteCreateService.ts
@@ -1030,7 +1030,7 @@ export class NoteCreateService implements OnApplicationShutdown {
 
 			// 自分自身のHTL
 			if (note.userHost == null) {
-				if (note.visibility !== 'specified' || !note.visibleUserIds.some(v => v === user.id) || note.userId === user.id) {
+				if (note.visibility !== 'specified' || note.visibleUserIds.some(v => v === user.id) || note.userId === user.id) {
 					await this.fanoutTimelineService.push(`homeTimeline:${user.id}`, note.id, this.meta.perUserHomeTimelineCacheMax, r);
 					if (note.fileIds.length > 0) {
 						await this.fanoutTimelineService.push(`homeTimelineWithFiles:${user.id}`, note.id, this.meta.perUserHomeTimelineCacheMax / 2, r);
