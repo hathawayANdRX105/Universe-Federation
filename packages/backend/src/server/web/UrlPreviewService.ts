ecbaab1c189450a5e4917a461578217a40c5cf53
lpHex <lpHex@users.noreply.github.com>
2026-06-06 18:05:25 +0000

    修复 URL 预览请求未使用受控网络代理

--- packages/backend/src/server/web/UrlPreviewService.ts
+++ packages/backend/src/server/web/UrlPreviewService.ts
@@ -371,17 +371,13 @@ export class UrlPreviewService {
 	}
 
 	private fetchSummary(url: string, meta: MiMeta, lang?: string): Promise<SummalyResult> {
-		const agent = this.config.proxy
-			? {
-				http: this.httpRequestService.httpAgent,
-				https: this.httpRequestService.httpsAgent,
-			}
-			: undefined;
-
 		return summaly(url, {
 			followRedirects: true,
 			lang: lang ?? 'ja-JP',
-			agent: agent,
+			agent: {
+				http: this.httpRequestService.httpAgent,
+				https: this.httpRequestService.httpsAgent,
+			},
 			userAgent: meta.urlPreviewUserAgent ?? undefined,
 			operationTimeout: meta.urlPreviewTimeout,
 			contentLengthLimit: meta.urlPreviewMaximumContentLength,
